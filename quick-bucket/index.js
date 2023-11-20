c8daf847282dcd8d193654ac6dfd5a84ae762aef
Kamran Linux <hassan.kamran107@gmail.com>
2023-11-21 04:23:52 +0530

    Update

--- quick-bucket/index.js
+++ quick-bucket/index.js
@@ -36,45 +36,45 @@ app.get('/quickBucket/watch/', waAuthenticateHeaderToken, watchStreamApi)
 
 
 
-  app.get('/video/:fileName', (req, res) => {
-    const { fileName } = req.params;
-    const videoPath = path.join(__dirname, 'uploads', fileName);
-    console.log(videoPath)
-    // Check if the file exists
-    if (fs.existsSync(videoPath)) {
-      const stat = fs.statSync(videoPath);
-      const fileSize = stat.size;
-      const range = req.headers.range;
+  // app.get('/video/:fileName', (req, res) => {
+  //   const { fileName } = req.params;
+  //   const videoPath = path.join(__dirname, 'uploads', fileName);
+  //   console.log(videoPath)
+  //   // Check if the file exists
+  //   if (fs.existsSync(videoPath)) {
+  //     const stat = fs.statSync(videoPath);
+  //     const fileSize = stat.size;
+  //     const range = req.headers.range;
   
-      // Parse the Range header to get start and end values for streaming
-      if (range) {
-        const parts = range.replace(/bytes=/, '').split('-');
-        const start = parseInt(parts[0], 10);
-        const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+  //     // Parse the Range header to get start and end values for streaming
+  //     if (range) {
+  //       const parts = range.replace(/bytes=/, '').split('-');
+  //       const start = parseInt(parts[0], 10);
+  //       const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
   
-        const chunksize = end - start + 1;
-        const file = fs.createReadStream(videoPath, { start, end });
-        const head = {
-          'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-          'Accept-Ranges': 'bytes',
-          'Content-Length': chunksize,
-          'Content-Type': 'video/mp4', // Adjust the Content-Type according to your video format
-        };
+  //       const chunksize = end - start + 1;
+  //       const file = fs.createReadStream(videoPath, { start, end });
+  //       const head = {
+  //         'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+  //         'Accept-Ranges': 'bytes',
+  //         'Content-Length': chunksize,
+  //         'Content-Type': 'video/mp4', // Adjust the Content-Type according to your video format
+  //       };
   
-        res.writeHead(206, head);
-        file.pipe(res);
-      } else {
-        const head = {
-          'Content-Length': fileSize,
-          'Content-Type': 'video/mp4', // Adjust the Content-Type according to your video format
-        };
-        res.writeHead(200, head);
-        fs.createReadStream(videoPath).pipe(res);
-      }
-    } else {
-      res.status(404).send('File not found');
-    }
-  });
+  //       res.writeHead(206, head);
+  //       file.pipe(res);
+  //     } else {
+  //       const head = {
+  //         'Content-Length': fileSize,
+  //         'Content-Type': 'video/mp4', // Adjust the Content-Type according to your video format
+  //       };
+  //       res.writeHead(200, head);
+  //       fs.createReadStream(videoPath).pipe(res);
+  //     }
+  //   } else {
+  //     res.status(404).send('File not found');
+  //   }
+  // });
 
 const PORT = 9999;
 app.listen(PORT, () => {
